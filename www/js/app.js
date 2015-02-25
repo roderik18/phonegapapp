cfff93b223c18de312d96be9f80116bbc932843d
Roderik Masure <roderikmasure@gmail.com>
2015-02-25 10:24:43 +0100

    niks
    daarom

---
+++ www/js/app.js
@@ -0,0 +1,56 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var slider = new PageSlider($('body'));
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        router.addRoute('', function () {
+            slider.slidePage(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function (id) {
+            service.findById(parseInt(id)).done(function (employee) {
+                slider.slidePage(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+
+    document.addEventListener('deviceready', function () {
+        FastClick.attach(document.body);
+        StatusBar.overlaysWebView(false);
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+    //function findByName() {
+    //    service.findByName($('.search-key').val()).done(function (employees) {
+    //        $('.content').html(employeeListTpl(employees));
+    //    });
+    //}
+
+    //function renderHomeView() {
+    //    $('body').html(homeTpl());
+    //    $('.search-key').on('keyup', findByName);
+    //}
+
+}());
